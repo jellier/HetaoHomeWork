d381a7fecc978c16bbc4b6448c1fbcaa0c68314c
jellier@163.com <jellier@163.com>
2020-11-12 08:48:13 +0800

    update for matplot pie

--- ShopCartMatplot.py
+++ ShopCartMatplot.py
@@ -1,9 +1,48 @@
+from os import path
 import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 
-labels = ['娱乐','育儿','饮食','房贷','交通','其它']
-sizes = [2,5,12,70,2,9]
-explode = (0,0,0,0.1,0,0)
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+d = path.dirname(__file__)
+string = open(path.join(d,  "image/shopcart_jieba.txt")).read()
+
+textlist = string.split(' ')
+print(textlist)
+child=['儿童', '宝宝', '青少年', '幼儿', '中大童', '小童', '男孩', '女孩', '12 ', '7 ', '15 ', '3 ', '4 ', '5', '亲子']
+adult = ['男', '女', '妈妈', '爸爸', '成人', '中老年']
+food =['早餐', '代餐 ', '饱腹 ', '食品 ', '五谷杂粮', '粗粮 ', '小吃', '主食', '零食', '奶酪 ', '烘焙', '海鲜', '糖果']
+toy =['滑板车 ', '闪光']
+book=['进口 ', '小说 ', '桥梁 ', '书', '英文原版 ', '全彩', '卡通', '启蒙 ', '绘本 ', '图画书 ', '新华 ', '正版', '数学 ', '书籍', '平装 ', '套装', '系列 ', '全套']
+cloth = ['套头毛衣 ', '线衣 ', '针织 ', '上衣', '纯棉', '羽绒服']
+family = ['户外 ', '手推车 ', '家居服']
+rword = '其他'
+counts={}
+for word in textlist:
+    # 去标点符号
+    if word == ' ':
+        continue
+    elif word in child or word in toy or word in book:
+        rword = '孩子'
+    elif word in adult or word in cloth:
+        rword = '大人'
+    elif word in food or word in family:
+        rword = '全家'
+
+    counts[rword] = counts.get(rword,  0) + 1
+print(counts)
+
+labels = counts.keys()
+sizes = counts.values()
+# 获取最大值的位置
+max_index = list(sizes).index(max(sizes))
+
+# explode：指定饼图某些部分的突出显示，即呈现爆炸式
+# explode = (0,0,0.1,0)
+explode_list = [0]*len(counts)
+explode_list[max_index] = 0.1
+explode = tuple(explode_list)
+
 plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=False,startangle=150)
-plt.title("饼图示例-8月份家庭支出")
+plt.title("老母亲的购物车")
 plt.show()
--- ShopCartWorldCloud.py
+++ ShopCartWorldCloud.py
@@ -14,8 +14,11 @@ text = open(path.join(d,  "image/shopcart.txt")).read()
 
 # 步骤2：使用jieba分词
 textlist = jieba.lcut(text)
-string = " ".join(textlist)
 print(textlist)
+string = " ".join(textlist)
+# 将分词存储到txt文件备用，注意：只能存str,不能存list
+f = open("image/shopcart_jieba.txt",'w')
+f.write(string)
 
 # 步骤3：设置一张词云图对象
 bg = np.array(Image.open("image/shop.jpg"))
@@ -35,26 +38,3 @@ plt.imshow(wordcloud,  interpolation="bilinear")
 plt.axis("off")
 # 显示图片
 plt.show()
-
-counts={}
-child=['儿童', '宝宝', '青少年', '幼儿', '中大童', '小童', '男孩', '女孩', '12 ', '7 ', '15 ', '3 ', '4 ', '5', '亲子']
-adult = ['男', '女', '妈妈', '爸爸', '成人', '中老年']
-food =['早餐', '代餐 ', '饱腹 ', '食品 ', '五谷杂粮', '粗粮 ', '小吃', '主食', '零食', '奶酪 ', '烘焙', '海鲜', '糖果']
-toy =['滑板车 ', '闪光']
-book=['进口 ', '小说 ', '桥梁 ', '书', '英文原版 ', '全彩', '卡通', '启蒙 ', '绘本 ', '图画书 ', '新华 ', '正版', '数学 ', '书籍', '平装 ', '套装', '系列 ', '全套']
-cloth = ['套头毛衣 ', '线衣 ', '针织 ', '上衣', '纯棉', '羽绒服']
-family = ['户外 ', '手推车 ', '家居服']
-rword = '其他'
-for word in textlist:
-    # 去标点符号
-    if word == ' ':
-        continue
-    elif word in child or word in toy or word in book:
-        rword = '孩子'
-    elif word in adult or word in cloth:
-        rword = '大人'
-    elif word in food or word in family:
-        rword = '全家'
-
-    counts[rword] = counts.get(rword,  0) + 1
-print(counts)
